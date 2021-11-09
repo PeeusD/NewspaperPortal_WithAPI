3544fc616357857feac420250ee3735cc41db4e8
PeeusD <2014pgmca076@nitjsr.ac.in>
2021-11-09 21:36:18 +0530

    added custom-api functionality

--- downloads/views.py
+++ downloads/views.py
@@ -1,35 +1,66 @@
 from django.shortcuts import render
-
+import json, requests
 # Create your views here.
+
+
+def call_func(strt,end):
+    res = requests.get('https://apiv2xxx.herokuapp.com/api')
+    if res.status_code == 200:
+        res = json.loads(res.text)
+        res = res['data']
+        dt ={}
+        
+    for i in range(strt,end):
+        dt[res[i]['date']] = res[i]['link']
+
+    return dt
+
+
+
 def businessline(request):
-    return render (request, 'downloads/busi.html')
+    dt = call_func(0,7)
+    return render (request, 'downloads/busi.html',{"dates":dt})
 
 def dec(request):
-    return render(request, 'downloads/dec.html')
+    dt = call_func(35,42)
+    return render(request, 'downloads/dec.html',{"dates":dt})
 
 def eco(request):
-    return render(request, 'downloads/eco.html')
+    dt = call_func(28,35)
+    return render(request, 'downloads/eco.html',{"dates":dt})
 
 def hintim(request):
-    return render(request, 'downloads/hin.html')
+    dt = call_func(49,56)
+    return render(request, 'downloads/hin.html',{"dates":dt})
 
 def fin(request):
-    return render(request, 'downloads/fin.html')
+    dt = call_func(7,14)
+    return render(request, 'downloads/fin.html',{"dates":dt})
 
 def indexp(request):
-    return render(request, 'downloads/ind.html')
+    dt = call_func(14,21)
+    return render(request, 'downloads/ind.html',{"dates":dt})
 
 def thehindu(request):
-    return render(request, 'downloads/th.html')
+
+    dt = call_func(0,7)
+    return render(request, 'downloads/th.html',{"dates":dt})
+
+
 
 def brills(request):
-    return render(request, 'downloads/brills.html')
+    dt = call_func(0,7)
+    return render(request, 'downloads/brills.html',{"dates":dt})
 
 def toi(request):
-    return render(request, 'downloads/toi.html')
+    dt = call_func(56,63)
+    
+    return render(request, 'downloads/toi.html',{"dates":dt})
 
 def tribune(request):
-    return render(request, 'downloads/tri.html')
+    dt = call_func(0,7)
+    
+    return render(request, 'downloads/tri.html',{"dates":dt})
 
 
 
