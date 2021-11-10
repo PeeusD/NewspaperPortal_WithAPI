09d5f8afc9428afb12f8af754355ca5a49d3a3d9
PeeusD <2014pgmca076@nitjsr.ac.in>
2021-11-10 19:47:06 +0530

    added comments and updated readme

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # NewspaperPortal_WithAPI
 This is the newspaper website designed in django which uses custom based api wriiten in flask for rendering different download links.
 ![alt text](https://github.com/PeeusD/NewspaperPortal_WithAPI/blob/main/git_pics/pic1.png?raw=true)
+### Home Page
 ![alt text](https://github.com/PeeusD/NewspaperPortal_WithAPI/blob/main/git_pics/pic2.png?raw=true)
+### About Page
 ![alt text](https://github.com/PeeusD/NewspaperPortal_WithAPI/blob/main/git_pics/pic3.png?raw=true)
+### Privacy Page
 ![alt text](https://github.com/PeeusD/NewspaperPortal_WithAPI/blob/main/git_pics/pic4.png?raw=true)
+### Contact Page
 ![alt text](https://github.com/PeeusD/NewspaperPortal_WithAPI/blob/main/git_pics/pic5.png?raw=true)
+### Download Page1
 ![alt text](https://github.com/PeeusD/NewspaperPortal_WithAPI/blob/main/git_pics/pic6.png?raw=true)
+### Download Page2
 ![alt text](https://github.com/PeeusD/NewspaperPortal_WithAPI/blob/main/git_pics/pic7.png?raw=true)
+### Download Links with details page1
 ![alt text](https://github.com/PeeusD/NewspaperPortal_WithAPI/blob/main/git_pics/pic8.png?raw=true)
+### Download Links with details page1
--- downloads/views.py
+++ downloads/views.py
@@ -3,62 +3,67 @@ import json, requests
 # Create your views here.
 
 
-def call_func(strt,end):
+def calling_api(strt,end):
+    #requesting api
     res = requests.get('https://apiv2xxx.herokuapp.com/api')
+    #checking status for response
+    dt = {}
     if res.status_code == 200:
+        #converting json response to dict obj
         res = json.loads(res.text)
         res = res['data']
-        dt ={}
-        
-    for i in range(strt,end):
-        dt[res[i]['date']] = res[i]['link']
-
+         
+        for i in range(strt,end):
+            #appending keys and values to new dt dict...
+            dt[res[i]['date']] = res[i]['link']
+    else:
+        dt['Server_temporarily_down_Please_try_again_later'] = 'https://tsparticles.github.io/404-templates/space/404.html'
     return dt
 
 
 
 def businessline(request):
-    dt = call_func(0,7)
+    dt = calling_api(0,7)
     return render (request, 'downloads/busi.html',{"dates":dt})
 
 def dec(request):
-    dt = call_func(35,42)
+    dt = calling_api(35,42)
     return render(request, 'downloads/dec.html',{"dates":dt})
 
 def eco(request):
-    dt = call_func(28,35)
+    dt = calling_api(28,35)
     return render(request, 'downloads/eco.html',{"dates":dt})
 
 def hintim(request):
-    dt = call_func(49,56)
+    dt = calling_api(49,56)
     return render(request, 'downloads/hin.html',{"dates":dt})
 
 def fin(request):
-    dt = call_func(7,14)
+    dt = calling_api(7,14)
     return render(request, 'downloads/fin.html',{"dates":dt})
 
 def indexp(request):
-    dt = call_func(14,21)
+    dt = calling_api(14,21)
     return render(request, 'downloads/ind.html',{"dates":dt})
 
 def thehindu(request):
 
-    dt = call_func(0,7)
+    dt = calling_api(0,7)
     return render(request, 'downloads/th.html',{"dates":dt})
 
 
 
 def brills(request):
-    dt = call_func(0,7)
+    dt = calling_api(0,7)
     return render(request, 'downloads/brills.html',{"dates":dt})
 
 def toi(request):
-    dt = call_func(56,63)
+    dt = calling_api(56,63)
     
     return render(request, 'downloads/toi.html',{"dates":dt})
 
 def tribune(request):
-    dt = call_func(0,7)
+    dt = calling_api(0,7)
     
     return render(request, 'downloads/tri.html',{"dates":dt})
 
